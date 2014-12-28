3af049d8ee781a65689773312e9b0de28c24a69f
pyankoff <pyankoff@gmail.com>
2014-12-29 00:53:33 +0300

    screen recognition

--- app.lua
+++ app.lua
@@ -8,6 +8,15 @@ torch.setdefaulttensortype('torch.FloatTensor')
 local network = 'icons97good.net'
 local model = torch.load(network, 'ascii')
 
+local file = io.open("filteredApps.txt", "r");
+local appsList = {}
+for line in file:lines() do
+  table.insert (appsList, line);
+end
+
+x = {31, 183, 335, 487}
+y = {89, 254, 420, 586, 753, 972}
+
 app:match("/", function(self)
   local file = self.params.upload
 
@@ -15,14 +24,18 @@ app:match("/", function(self)
   save:write(file.content)  
   save:close()
   
-  local huy = torch.Tensor(20)
-  local img = image.load("img.jpg")
-  img = image.scale(img, 32, 32)
+  local inputImage = image.load("img.jpg")
+  for i = 1, 4 do
+    for j = 1, 6 do
+      icon = inputImage[{{}, {y[j],y[j]+120}, {x[i],x[i]+120}}]
+      img = image.scale(icon, 32, 32)
 
-  output = model:forward(img)
-  y, result = torch.max(output, 1)
+      output = model:forward(img)
+      k, result = torch.max(output, 1)
+      print(appsList[result[1]+1])
+    end
+  end
 
-  return ""..result[1]
 end)
 
 return app
