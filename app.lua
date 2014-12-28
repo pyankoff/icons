a2c846c46977a769c5bc44aa2738fe7426828226
Ubuntu <ubuntu@ip-172-31-33-137.us-west-2.compute.internal>
2014-12-28 19:42:18 +0000

    shite

--- app.lua
+++ app.lua
@@ -1,18 +1,20 @@
 local lapis = require("lapis")
 local app = lapis.Application()
-local torch = require 'torch'
-local nn = require 'nn'
-local image = require 'image'
+local torch = require("torch")
+require("nn")
+require("image")
 
 app:match("/", function(self)
   local file = self.params.upload
 
   local save = io.open("img.jpg", "w")
   save:write(file.content)  
+  save:close()
   
+  local huy = torch.Tensor(20)
   local img = image.load("img.jpg")
 
-  print(img:size())
+  return ""..img:size()[2]
 end)
 
 return app
